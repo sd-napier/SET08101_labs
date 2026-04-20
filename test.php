37ce92c8ccc7ed6810bf4ef1050c9a24ab1409b9
sd-napier <40785797@live.napier.ac.uk>
2026-04-20 13:16:54 +0100

    Added first PHP code from Lab 7

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+echo "Hello from PHP!";
+?>
+
+<?php
+$name = "Alice";
+$age = 25;
+$isStudent = true;
+
+echo "Name: $name<br>";
+echo "Age: $age<br>";
+echo "Student: " . ($isStudent ? "Yes" : "No");
+?>
